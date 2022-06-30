de1edf1749f1f0c8f902f3e0370128f13a77b93a
Apurav Sharma <98868273+apurav04@users.noreply.github.com>
2022-07-01 03:27:04 +0530

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner s= new Scanner(System.in);
+        char ch='e';
+        int ascii = ch;
+        System.out.println(giveascii(ascii));
+
+    }
+    public static int giveascii(int ascii){
+        return ascii;
+    }
+}
